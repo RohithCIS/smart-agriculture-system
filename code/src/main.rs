5c38b880ced2d13f67630a38d337cf1d848563ad
Rohith Balaji <me@therohith.com>
2020-09-03 22:10:18 +0530

    Added Default Target

--- code/src/main.rs
+++ code/src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    primitives::{Circle, Rectangle, Triangle},
+    style::{PrimitiveStyle, TextStyle},
+};
+
+use cortex_m_rt::{entry, exception, ExceptionFrame};
+use panic_halt as _;
+use ssd1306::{prelude::*, Builder, I2CDIBuilder};
+use stm32f1xx_hal::{
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    prelude::*,
+    stm32,
+};
+
+#[entry]
+fn main() -> ! {
+    let text_style = TextStyle::new(Font6x8, BinaryColor::On);
+
+    let dp = stm32::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000.hz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let interface = I2CDIBuilder::new().init(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
+
+    disp.init().unwrap();
+
+    let text = "Micro Controller ECE5042";
+    let width = text.len() as i32 * 6;
+    Text::new(text, Point::new(64 - width / 2, 40))
+        .into_styled(text_style)
+        .draw(&mut disp).unwrap();
+
+    disp.flush().unwrap();
+
+    loop {}
+}
+
+#[exception]
+fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
 }
