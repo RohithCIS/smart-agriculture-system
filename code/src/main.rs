2872aba2cb02e90cb8bfcc591641e05d2b158fc2
Rohith Balaji <me@therohith.com>
2020-09-11 22:54:40 +0530

    Opt-Level update considering .rodata size and OLED Display Hookup

--- code/src/main.rs
+++ code/src/main.rs
@@ -3,24 +3,24 @@
 
 use panic_halt as _;
 
-use embedded_graphics::{
-    egrectangle, egtext,
-    fonts::Font6x8,
-    pixelcolor::BinaryColor,
-    prelude::*,
-    style::{PrimitiveStyle, TextStyle},
-};
-
+use core::fmt::Write;
 use cortex_m_rt::{entry, exception, ExceptionFrame};
 use cortex_m_semihosting::hprintln;
-use ssd1306::{prelude::*, Builder, I2CDIBuilder};
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    style::{TextStyle},
+};
 use embedded_hal::digital::v2::OutputPin;
+use heapless::String;
+use ssd1306::{prelude::*, Builder, I2CDIBuilder};
 use stm32f1xx_hal::{
     adc, delay,
     i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
     prelude::*,
     stm32,
-    pac,
 };
 
 mod libraries;
@@ -28,10 +28,14 @@ mod libraries;
 #[entry]
 fn main() -> ! {
     let text_style = TextStyle::new(Font6x8, BinaryColor::On);
-    let thick_stroke = PrimitiveStyle::with_stroke(BinaryColor::On, 3);
+
+    // Device Peripherals
     let dp = pac::Peripherals::take().unwrap();
+
+    // Core Peripherals
     let cp = stm32::CorePeripherals::take().unwrap();
 
+    // Get the FLASH memory structure
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
 
@@ -42,8 +46,10 @@ fn main() -> ! {
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
-    let mut pc2 = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
+    // Configure PA2 as Relay Control
+    let mut pa2 = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
 
+    // Initialise the ADC1 at PA1
     let mut adc1 = adc::Adc::adc1(dp.ADC1, &mut rcc.apb2, clocks);
 
     let mut delay = delay::Delay::new(cp.SYST, clocks);
@@ -51,60 +57,34 @@ fn main() -> ! {
     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
     let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
 
-    // let i2c = BlockingI2c::i2c1(
-    //     dp.I2C1,
-    //     (scl, sda),
-    //     &mut afio.mapr,
-    //     Mode::Fast {
-    //         frequency: 400_000.hz(),
-    //         duty_cycle: DutyCycle::Ratio2to1,
-    //     },
-    //     clocks,
-    //     &mut rcc.apb1,
-    //     1000,
-    //     10,
-    //     1000,
-    //     1000,
-    // );
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000.hz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
 
-    // let interface = I2CDIBuilder::new().init(i2c);
-    // let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
+    let interface = I2CDIBuilder::new().init(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
 
-    // disp.init().unwrap();
-
-    // egtext!(text = "ECE5042", top_left = (5, 5), style = text_style)
-    //     .draw(&mut disp)
-    //     .unwrap();
-
-    // egtext!(
-    //     text = "Microcontroller",
-    //     top_left = (5, 15),
-    //     style = text_style
-    // )
-    // .draw(&mut disp)
-    // .unwrap();
-
-    // egtext!(text = "Project", top_left = (5, 25), style = text_style)
-    //     .draw(&mut disp)
-    //     .unwrap();
-
-    // // libraries::oled::display_text(disp, "test", 5, 45);
-
-    // egrectangle!(
-    //     top_left = (64, 5),
-    //     bottom_right = (75, 15),
-    //     style = thick_stroke
-    // )
-    // .draw(&mut disp).unwrap();
-
-    // disp.flush().unwrap();
+    disp.init().unwrap();
 
     hprintln!("Waiting on the sensor...").unwrap();
     delay.delay_ms(3000_u16);
 
-    let mut pa4 = gpioa.pa4.into_open_drain_output(&mut gpioa.crl);
+    // let mut pa4 = gpioa.pa4.into_open_drain_output(&mut gpioa.crl);
     let mut pa3 = gpioa.pa3.into_analog(&mut gpioa.crl);
 
+    // let mut reading = dht11::Reading::read(&mut delay, &mut pa4).unwrap();
     // match dht11::Reading::read(&mut delay, &mut pa4) {
     //     Ok(dht11::Reading {
     //         temperature,
@@ -113,15 +93,44 @@ fn main() -> ! {
     //     Err(e) => hprintln!("Error {:?}", e).unwrap(),.unwrap()
     // }
 
+    let mut moisture_string: String<heapless::consts::U32> = String::new();
+    let mut relay_string: String<heapless::consts::U32> = String::new();
+
     loop {
+        moisture_string.clear();
+        relay_string.clear();
         let data: u16 = adc1.read(&mut pa3).unwrap();
         if libraries::moisture_sensor::get_reading(data) {
             hprintln!("Low Moisture Turning on Pump, Analog {}", data).unwrap();
-            pc2.set_high().unwrap();
+            pa2.set_high().unwrap();
+            write!(relay_string, "Pump On").unwrap();
         } else {
             hprintln!("High Moisture Turning off Pump, Analog {}", data).unwrap();
-            pc2.set_low().unwrap();
+            pa2.set_low().unwrap();
+            write!(relay_string, "Pump Off").unwrap();
         }
+
+        write!(moisture_string, "Moisture Analog {}", data).unwrap();
+
+        disp.clear();
+        Text::new(moisture_string.as_str(), Point::new(5, 5))
+            .into_styled(text_style)
+            .draw(&mut disp)
+            .unwrap();
+        Text::new(relay_string.as_str(), Point::new(5, 15))
+            .into_styled(text_style)
+            .draw(&mut disp)
+            .unwrap();
+        Text::new("Temperature 38°C", Point::new(5, 25))
+            .into_styled(text_style)
+            .draw(&mut disp)
+            .unwrap();
+        Text::new("Humidity 50%", Point::new(5, 35))
+            .into_styled(text_style)
+            .draw(&mut disp)
+            .unwrap();
+
+        disp.flush().unwrap();
 
         delay.delay_ms(1000u16);
     }
